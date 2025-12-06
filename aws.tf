af94a78f05f485b3fe3f0b9868336bc1990535c0
drewpypro <83561525+drewpypro@users.noreply.github.com>
2025-12-06 14:46:30 -0700

    testing removal of AWS SG

--- aws.tf
+++ aws.tf
@@ -30,10 +30,10 @@ locals {
 #   }
 # }
 
-output "security_group_ids" {
-  value = {
-    for name, sg in aws_security_group.sgs :
-    name => sg.id
-  }
-  description = "Map of all security group names to their IDs"
-}
+# output "security_group_ids" {
+#   value = {
+#     for name, sg in aws_security_group.sgs :
+#     name => sg.id
+#   }
+#   description = "Map of all security group names to their IDs"
+# }
