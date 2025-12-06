c436d8a19c679b0ac34b404992c41efd9d231dff
drewpypro <83561525+drewpypro@users.noreply.github.com>
2025-12-06 14:48:13 -0700

    testing add and destroy

--- aws.tf
+++ aws.tf
@@ -19,21 +19,21 @@ locals {
     ]
 }
 
-# resource "aws_security_group" "sgs" {
-#   for_each = toset(local.security_groups)
-#   name        = each.value
-#   description = "Managed by Terraform"
-#   vpc_id      = data.aws_vpc.default.id
+resource "aws_security_group" "sgs" {
+  for_each = toset(local.security_groups)
+  name        = each.value
+  description = "Managed by Terraform"
+  vpc_id      = data.aws_vpc.default.id
 
-#   tags = {
-#     Name = each.value
-#   }
-# }
+  tags = {
+    Name = each.value
+  }
+}
 
-# output "security_group_ids" {
-#   value = {
-#     for name, sg in aws_security_group.sgs :
-#     name => sg.id
-#   }
-#   description = "Map of all security group names to their IDs"
-# }
+output "security_group_ids" {
+  value = {
+    for name, sg in aws_security_group.sgs :
+    name => sg.id
+  }
+  description = "Map of all security group names to their IDs"
+}
